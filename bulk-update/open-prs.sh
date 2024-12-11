74f7452fd1ca0f836361a0c92ac89da7e803aa83
Oleksandra Tkachenko <tinkooladik@gmail.com>
2024-12-11 10:59:29 -0500

    Test/filter labels (#152)
    * test
    
    * check if branch updated; catch create pr errors
    
    * UPDATE PR ONLY IF something changed

--- bulk-update/open-prs.sh
+++ bulk-update/open-prs.sh
@@ -66,6 +66,12 @@ while IFS= read -r line || [ -n "$line" ]; do
   esac
 done < "$CONFIG_FILE"
 
+# Check if BRANCH is updated
+if [[ "$BRANCH" == "alex/RBMN-XXXXX-update-repos" ]]; then
+  echo "Using template branch 'alex/RBMN-XXXXX-update-repos'. Did you forget to update the config file? 🔫😾😾😾"
+  exit 0
+fi
+
 PR_DESCRIPTION=$(echo -e "$PR_DESCRIPTION" | sed -e :a -e '/^\n*$/{$d;N;};/\n$/ba')
 
 # Validate input
@@ -90,6 +96,22 @@ cleanup() {
     echo "Cleaning up $REPO_DIR"
     rm -rf "$REPO_DIR"
   fi
+}
+
+update_pr() {
+    local success_msg="$1"
+    local error_msg="$2"
+
+    echo "Updating existing PR: $PR_URL"
+
+    if gh pr edit "$BRANCH" \
+                --title "$PR_TITLE" \
+                --body "$PR_DESCRIPTION"; then
+                PR_LINKS+=("$PR_URL ($success_msg)")
+    else
+        echo "Failed to update PR $PR_URL 😿"
+        FAILED_REPOS+=("$REPO ($error_msg)")
+    fi
 }
 
 # Set up a trap to call cleanup when the script exits
@@ -154,10 +176,34 @@ for REPO in "${REPOS[@]}"; do
       fi
   done
 
+  # Retrieve PR information
+  PR_INFO=$(gh pr view "$BRANCH" --json url,state,title,body --jq '{url: .url, state: .state, title: .title, description: .body}' 2>/dev/null || true)
+
+  # Parse the JSON response
+  PR_URL=$(echo "$PR_INFO" | jq -r '.url' 2>/dev/null)
+  PR_STATE=$(echo "$PR_INFO" | jq -r '.state' 2>/dev/null)
+
+  # Check if pr title / description were updated in config file
+  OLD_PR_TITLE=$(echo "$PR_INFO" | jq -r '.title' 2>/dev/null | xargs)
+  OLD_PR_DESCRIPTION=$(echo "$PR_INFO" | jq -r '.description' 2>/dev/null)
+  OLD_PR_DESCRIPTION=$(echo -e "$OLD_PR_DESCRIPTION" | sed -e :a -e '/^\n*$/{$d;N;};/\n$/ba')
+  if [[ "$OLD_PR_TITLE" != "$PR_TITLE" || "$OLD_PR_DESCRIPTION" != "$PR_DESCRIPTION" ]]; then
+    PR_DETAILS_UPDATED=true
+  else
+    PR_DETAILS_UPDATED=false
+  fi
+
   # Commit and push changes
   git add .
   git commit -m "$COMMIT_MESSAGE" || {
     echo "No changes to commit";
+
+    # Update PR description if needed and exit script
+    if [[ -n "$PR_URL" && "$PR_STATE" != "CLOSED" && "$PR_DETAILS_UPDATED" == "true" ]]; then
+        update_pr "no changes to commit, updated title / description" \
+            "no changes to commit, failed to update PR $PR_URL"
+        cd ..; cleanup; continue;
+    fi
     FAILED_REPOS+=("$REPO (no changes to commit)");
     cd ..; cleanup; continue; }
 
@@ -169,33 +215,23 @@ for REPO in "${REPOS[@]}"; do
     cd ..; cleanup; continue;
   }
 
-  PR_INFO=$(gh pr view "$BRANCH" --json url,state --jq '{url: .url, state: .state}' 2>/dev/null || true)
-  PR_URL=$(echo "$PR_INFO" | jq -r '.url' 2>/dev/null)
-  PR_STATE=$(echo "$PR_INFO" | jq -r '.state' 2>/dev/null)
-
   if [[ -n "$PR_URL" && "$PR_STATE" != "CLOSED" ]]; then
-    echo "Updating existing PR: $PR_URL"
-    if gh pr edit "$BRANCH" \
-        --title "$PR_TITLE" \
-        --body "$PR_DESCRIPTION"; then
-        PR_LINKS+=("$PR_URL (updated)")
-    else
-        echo "Failed to update PR $PR_URL 😿"
-        FAILED_REPOS+=("$REPO (failed to update PR $PR_URL)")
-    fi
+    update_pr "updated" \
+        "failed to update PR $PR_URL"
   else
     echo "Opening a new PR"
     PR_URL=$(gh pr create \
         --title "$PR_TITLE" \
         --body "$PR_DESCRIPTION" \
         --base main \
-        --head "$BRANCH" 2>/dev/null)
+        --head "$BRANCH" 2>/dev/null) || {
+              echo "Failed to create PR for repo $REPO 😿";
+              FAILED_REPOS+=("$REPO (failed to create PR)");
+              cd ..; cleanup; continue;
+           }
 
     if [[ $? -eq 0 && -n "$PR_URL" ]]; then
-        PR_LINKS+=("$PR_URL")
-    else
-        echo "Failed to create PR for $REPO 😿"
-        FAILED_REPOS+=("$REPO (failed to create PR)")
+        PR_LINKS+=("$PR_URL");
     fi
   fi
 
